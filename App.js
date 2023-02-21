75fb4c4d90d2cff342f410e7641a605fff23b88f
github username <github email address>
2023-02-21 16:36:29 -0500

    adds bottom tab icons to bottom tab bar

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import styles from './src/components/Styles'
 import Home from './src/components/Home'
 import SermonsNavBar from './src/components/SermonsNavBar'
@@ -11,15 +12,12 @@ import FBLogo from './src/components/Logo'
 
 
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Home/>
       <StatusBar style='auto'/>
-      <Button
-        title='Sermons'
-        onPress={() => navigation.navigate('Sermons')}
-      />
+      
     </View>
   )
 }
@@ -36,6 +34,7 @@ const Sermons = () => {
 
 // const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const homeIcon = <FontAwesome5 name={'home'}/>
 
 export default function App() {
   return (
@@ -50,9 +49,23 @@ export default function App() {
         <Tab.Screen 
           name='Home' 
           component={HomeScreen}
-          options={{ headerRight: (props) => <FBLogo {...props} /> }}
+          options={{ 
+            headerRight: (props) => <FBLogo {...props} />,
+            tabBarIcon: () => (
+              <FontAwesome5 name='home' size={30} />
+            )
+            }}
         />
-        <Tab.Screen name='Sermons' component={Sermons} />
+        <Tab.Screen 
+          name='Sermons' 
+          component={Sermons} 
+          options={{
+            headerRight: (props) => <FBLogo {...props} />,
+            tabBarIcon: () => (
+              <FontAwesome5 name='church' size={30} />
+            )
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
     
